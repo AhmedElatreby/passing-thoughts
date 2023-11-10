4dfa4df971bf9a5fd1e5c3134d42f2957add4f6f
ahmed El-Atreby <ahmedsport@hotmail.com>
2023-11-10 17:01:30 +0000

    fc

---
+++ src/AddThoughtForm.js
@@ -0,0 +1,36 @@
+import { generateId, getNewExpirationTime } from "./utilities";
+import React, { useState } from "react";
+
+export function AddThoughtForm(props) {
+  const [text, setText] = useState("");
+  const handleTextChange = ({ target }) => {
+    const { value } = target;
+    setText(value);
+  };
+
+  const handleSubmit = (event) => {
+    if (text.length > 0) {
+      event.preventDefault();
+      const thought = {
+        id: generateId(),
+        text: text,
+        expiresAt: getNewExpirationTime(),
+      };
+      props.addThought(thought);
+      setText("");
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="AddThoughtForm">
+      <input
+        onChange={handleTextChange}
+        value={text}
+        type="text"
+        aria-label="What's on your mind?"
+        placeholder="What's on your mind?"
+      />
+      <input type="submit" value="Add" />
+    </form>
+  );
+}
